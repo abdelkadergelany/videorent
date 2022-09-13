f3dbf5f0f0cd0097f57da5684c91f48664594028
aly-abdelkader-iut-cse-4301 <abdelkadergelany25@gmail.com>
2022-09-13 22:40:11 +0900

    Setting up Url Parameters

--- movies/views.py
+++ movies/views.py
@@ -7,3 +7,7 @@ from .models import Movie
 def index(request):
     movies = Movie.objects.all()
     return render(request, 'movies/index.html', {'movies': movies})
+
+
+def detail(request, movie_id):
+    pass
