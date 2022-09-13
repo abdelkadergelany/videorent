bbd86e80db4673f63de5f0615eda993c681c5c6a
aly-abdelkader-iut-cse-4301 <abdelkadergelany25@gmail.com>
2022-09-14 00:07:58 +0900

    Adding the Homepage

--- videorent/urls.py
+++ videorent/urls.py
@@ -16,9 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from api.models import MovieRessource
+from . import views
 
 movie_resource = MovieRessource()
 urlpatterns = [
+    path('', views.home),
     path('admin/', admin.site.urls),
     path('movies/', include('movies.urls')),
     path('api/', include(movie_resource.urls))
